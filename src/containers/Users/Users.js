2a0c9e3bb9f7d45ce44c570358fb9ebab52787ad
snt_dusan_bekovic <dbekovic@smartnet.rs>
2020-11-02 01:24:37 +0100

    latest

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -1,41 +1,78 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './Users.module.scss';
 import Button from '../../components/Button';
 import { withRouter } from 'react-router-dom';
-import { useMount, useUpdate } from '../../hooks';
+import { useUpdate } from '../../hooks';
 import { connect } from 'react-redux';
 import {
     usersData
 } from '../../selectors';
 import * as UsersActions from '../../redux/actions/usersAction';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
 
-const Users = ({getUsers, users}) => {
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+const Users = ({ getUsers, users }) => {
     const [userList, setUserList] = useState([]);
+
+    const classes = useStyles();
 
     const getUsersFromAPI = () => {
         getUsers();
     }
-    useUpdate(()=>{
+    useUpdate(() => {
         setUserList(users);
-    },[users]);
+    }, [users]);
     return (
-        <>
+        <Container>
             <h1 className={styles.users}>
                 Users
             </h1>
-        
-            <ul>
-                {userList ? userList.map(user =>{
-                    return <li>
-                        {user.name}
-                    </li>
-                }) : null}
-            </ul>
-            
+
+                <TableContainer component={Paper}>
+                    <Table className={classes.table} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell align="right">Username</TableCell>
+                                <TableCell align="right">Email</TableCell>
+                                <TableCell align="right">Phone</TableCell>
+                                <TableCell align="right">Website</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {users ? users.map((row) => (
+                                <TableRow key={row.name}>
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.username}</TableCell>
+                                    <TableCell align="right">{row.email}</TableCell>
+                                    <TableCell align="right">{row.phone}</TableCell>
+                                    <TableCell align="right">{row.website}</TableCell>
+                                </TableRow>
+                            )) : 'Currently no users'}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+
             <Button onClick={getUsersFromAPI} className={styles.buttonCustom}>
                 Get Users
             </Button>
-        </>
+        </Container>
     )
 }
 
--- src/redux/actions/homeAction.js
+++ src/redux/actions/homeAction.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 export const GET_POSTS = 'GET_POSTS';
 
 export const getPosts = () => async dispatch => {
     try {
-        const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        dispatch({
-            type: GET_POSTS,
-            data: res.data
-        })
+        // const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        // dispatch({
+        //     type: GET_POSTS,
+        //     data: res.data
+        // })
     } catch(err) {
 
     }
